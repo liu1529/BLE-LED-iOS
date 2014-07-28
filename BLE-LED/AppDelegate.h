ba4ea6a42c4dbe52329dd5f62586efb4a18c0492
xlliu <xlliu@xlliudeMac.local>
2014-07-28 16:37:42 +0800

    Initial Commit

---
+++ BLE-LED/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BLE-LED
+//
+//  Created by xlliu on 14-7-28.
+//  Copyright (c) 2014年 jiuzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BLE-LED/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BLE-LED
+//
+//  Created by xlliu on 14-7-28.
+//  Copyright (c) 2014年 jiuzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
